f2fa525f3bc92e57a42d8a5b47fdbaa820d2e42c
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-07-02 01:28:24 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,93 @@
+/**
+ * -------------------------------------------------------
+ * SMS Hindi Shayari
+ * User Routes
+ * -------------------------------------------------------
+ */
 
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const userController = require("../controllers/user");
+
+const {
+    isAuthenticated
+} = require("../middleware/auth");
+
+/* ==================================
+   Dashboard
+================================== */
+
+router.get(
+    "/dashboard",
+    isAuthenticated,
+    userController.dashboard
+);
+
+/* ==================================
+   Profile
+================================== */
+
+router.get(
+    "/profile",
+    isAuthenticated,
+    userController.profile
+);
+
+/* ==================================
+   Update Profile
+================================== */
+
+router.post(
+    "/profile",
+    isAuthenticated,
+    userController.updateProfile
+);
+
+/* ==================================
+   My Favorites
+================================== */
+
+router.get(
+    "/favorites",
+    isAuthenticated,
+    userController.favorites
+);
+
+/* ==================================
+   My Downloads
+================================== */
+
+router.get(
+    "/downloads",
+    isAuthenticated,
+    userController.downloads
+);
+
+/* ==================================
+   Change Password
+================================== */
+
+router.post(
+    "/change-password",
+    isAuthenticated,
+    userController.changePassword
+);
+
+/* ==================================
+   Delete Account
+================================== */
+
+router.post(
+    "/delete-account",
+    isAuthenticated,
+    userController.deleteAccount
+);
+
+/* ==================================
+   Export
+================================== */
+
+module.exports = router;
