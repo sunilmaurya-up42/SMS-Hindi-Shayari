b83a839d7663cea776e505b2c23e7babd3d7d732
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-07-01 17:29:11 +0530

    Update health.controller.js

--- backend/src/controllers/health.controller.js
+++ backend/src/controllers/health.controller.js
@@ -1,10 +1,19 @@
-export const healthCheck = async (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "SMS Hindi Shayari API is running.",
-    version: "v1",
-    environment: process.env.NODE_ENV || "development",
-    uptime: process.uptime(),
-    timestamp: new Date().toISOString()
-  });
+import mongoose from "mongoose";
+import ApiResponse from "../utils/ApiResponse.js";
+
+export const healthCheck = (req, res) => {
+  const database =
+    mongoose.connection.readyState === 1
+      ? "Connected"
+      : "Disconnected";
+
+  return res.status(200).json(
+    new ApiResponse(200, "SMS Hindi Shayari API is running.", {
+      version: "v1",
+      environment: process.env.NODE_ENV,
+      database,
+      uptime: process.uptime(),
+      timestamp: new Date().toISOString()
+    })
+  );
 };
